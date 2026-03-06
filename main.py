52ef778514c60faf4065c0a8aab4f22b327e1750
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2026-03-06 18:29:15 +0530

    fix: error

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import torch
 import os
-import tempfile
+import soundfile as sf
+from scipy.signal import resample_poly
+from dotenv import load_dotenv
 from transformers import AutoModel
 from diarizen.pipelines.inference import DiariZenPipeline
 
-from librosa import load as libr_load
-
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 # -------------------------------
 # Load models at startup
@@ -16,7 +18,9 @@ device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("
 print("Loading ASR model...")
 
 asr_model = AutoModel.from_pretrained(
-    "ai4bharat/indic-conformer-600m-multilingual", trust_remote_code=True
+    "ai4bharat/indic-conformer-600m-multilingual",
+    trust_remote_code=True,
+    token=HF_TOKEN,
 )
 asr_model = asr_model.to(device)
 
@@ -30,7 +34,7 @@ diar_pipeline.embedding_batch_size = 16
 diar_pipeline.segmentation_batch_size = 16
 
 # -------------------------------
-# Pipeline Class (simplified)
+# Pipeline Class
 # -------------------------------
 
 
@@ -43,12 +47,9 @@ class DiCoIndicPipeline:
 
     def decode_audio(self, wav_file, rttm_segments, target_sr=16000):
 
-        # ---- Load audio ----
         audio, sr = sf.read(wav_file, dtype="float32")
-        # check if Mono
         if audio.ndim == 2:
             audio = audio.mean(axis=1)
-        # Resample
         if sr != target_sr:
             audio = resample_poly(audio, target_sr, sr)
             sr = target_sr
@@ -64,20 +65,15 @@ class DiCoIndicPipeline:
             segment = audio[start:end]
 
             if len(segment) == 0:
-                transcription_text = ""
                 continue
 
-            # ---- Convert to torch ----
             waveform = torch.from_numpy(segment).unsqueeze(0).to(self.device)
 
-            # Pad if too short (minimum length for model compatibility)
             min_length = 512
-
             if waveform.shape[1] < min_length:
                 padding = min_length - waveform.shape[1]
                 waveform = torch.nn.functional.pad(waveform, (0, padding))
 
-            # Transcribe using model directly
             with torch.no_grad():
                 transcription = self.asr_model(waveform, "hi", "rnnt")
 
@@ -103,10 +99,8 @@ class DiCoIndicPipeline:
         diar_output = self.diarization_pipeline(audio_path)
 
         segments = []
-
         for speaker in diar_output.labels():
             timeline = diar_output.label_timeline(speaker)
-
             for segment in timeline:
                 segments.append(
                     {"speaker": speaker, "start": segment.start, "end": segment.end}
@@ -114,10 +108,7 @@ class DiCoIndicPipeline:
 
         segments = sorted(segments, key=lambda x: x["start"])
 
-        transcripts = self.decode_audio(
-            audio_path,
-            segments,
-        )
+        transcripts = self.decode_audio(audio_path, segments)
 
         return transcripts
 
@@ -126,6 +117,6 @@ pipeline = DiCoIndicPipeline(
     asr_model, diarization_pipeline=diar_pipeline, device=device
 )
 
-audio_file = "179729.wav"  # path to uploaded audio
+audio_file = "179729.wav"
 result = pipeline.transcribe(audio_file)
 print(result)
